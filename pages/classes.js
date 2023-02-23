93caccd75600724fdfb01ac8571ed04cf2474ac8
hlila86 <hamdianoo@gmail.com>
2023-02-23 20:34:28 +0000

    commit 5

--- pages/classes.js
+++ pages/classes.js
@@ -1,47 +1,38 @@
-import Layout from '@/components/Layout'
-import React from 'react'
-import styles from '../styles/classes.module.css'
+import Layout from "@/components/Layout";
+import React from "react";
+import styles from "../styles/classes.module.css";
 
 const classes = () => {
   return (
-
     <Layout>
-
-         <div className={styles.container}>
-
-         <div className={styles.card}>
-         <img
-        className="d-block w-100"
-        src="/img/yoga.jpg"
-        alt="First slide"
-      />
-            <h1 className={styles.cardTitle}>Yoga</h1>
-
-         </div>
-         <div className={styles.card}>
-         <img
-        className="d-block w-100"
-        src="/img/yoga.jpg"
-        alt="First slide"
-      />
-            <h1 className={styles.cardTitle}>Yoga</h1>
-
-         </div>
-         <div className={styles.card}>
-         <img
-        className="d-block w-100"
-        src="/img/yoga.jpg"
-        alt="First slide"
-      />
-            <h1 className={styles.cardTitle}>Yoga</h1>
-
-         </div>
-
-
-         </div>
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <img
+            className="d-block w-100"
+            src="/img/yoga.jpg"
+            alt="First slide"
+          />
+          <h1 className={styles.cardTitle}>Yoga</h1>
+        </div>
+        <div className={styles.card}>
+          <img
+            className="d-block w-100"
+            src="/img/yoga.jpg"
+            alt="First slide"
+          />
+          <h1 className={styles.cardTitle}>Yoga</h1>
+        </div>
+        <div className={styles.card}>
+          <img
+            className="d-block w-100"
+            src="/img/yoga.jpg"
+            alt="First slide"
+          />
+          <h1 className={styles.cardTitle}>Yoga</h1>
+        </div>
+      </div>
     </Layout>
-   
-  )
-}
+  );
+};
 
-export default classes
+export default classes;
--- pages/team.js
+++ pages/team.js
@@ -1,12 +1,29 @@
 import Layout from '@/components/Layout'
 import React from 'react'
+import styles from '../styles/Team.module.css'
 
 const team = () => {
   return (
 
     <Layout>
 
-         <div>team</div>
+         <div className={styles.container}>
+
+         <div className={styles.card}>
+  <img src="img/yoga.jpg" alt="John"  />
+  <h1>John Doe</h1>
+  <p className={styles.title}>CEO & Founder, Example</p>
+  <p>Harvard University</p>
+  <a href="#"><i class="fa fa-dribbble"></i></a>
+  <a href="#"><i class="fa fa-twitter"></i></a>
+  <a href="#"><i class="fa fa-linkedin"></i></a>
+  <a href="#"><i class="fa fa-facebook"></i></a>
+  <p><button className={styles.btn}>Contact</button></p>
+</div>
+
+
+
+         </div>
     </Layout>
    
   )
